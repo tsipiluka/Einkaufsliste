fc9705d69672108292fd158bfa6cc6c115ff8544
DWsRekt <thedavidlord@yahoo.de>
2022-10-31 15:18:18 +0100

    Changed App Modules, fixed html loading bug

--- einkaufsliste/src/app/app.module.ts
+++ einkaufsliste/src/app/app.module.ts
@@ -1,18 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { OAuthModule } from 'angular-oauth2-oidc';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { EinkaufslistenOverviewComponent } from './einkaufslisten-overview/einkaufslisten-overview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    EinkaufslistenOverviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OAuthModule.forRoot(),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
