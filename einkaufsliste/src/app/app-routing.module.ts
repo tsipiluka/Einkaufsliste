92169f3e441aab156fdc87048abb0cfcffb7d99a
DWsRekt <thedavidlord@yahoo.de>
2022-11-01 17:07:26 +0100

    Added app routing

--- einkaufsliste/src/app/app-routing.module.ts
+++ einkaufsliste/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EinkaufslistenOverviewComponent } from './einkaufslisten-overview/einkaufslisten-overview.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/einkaufslisten-overview', pathMatch: 'full' },
+  { path: 'einkaufslisten-overview', component: EinkaufslistenOverviewComponent},
+  { path: 'login', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
