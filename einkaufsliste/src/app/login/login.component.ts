f30eb61917cbf8447c9836b2c4ecac0f7c3e4ab5
DWsRekt <thedavidlord@yahoo.de>
2022-11-01 17:06:11 +0100

    Changed Login Component

--- einkaufsliste/src/app/login/login.component.ts
+++ einkaufsliste/src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { GoogleApiService, UserInfo } from '../google-api.service';
+import { Router } from '@angular/router';
+import { GoogleUserInfo } from '../entities/google-user-info.model';
+import { GoogleApiService } from '../google-api.service';
+import { LoginService } from './service/login.service';
 
 
 @Component({
@@ -10,18 +13,38 @@ import { GoogleApiService, UserInfo } from '../google-api.service';
 
 })
 export class LoginComponent implements OnInit {
-  
-  constructor(private readonly googleApi: GoogleApiService) {}
+
+  userInfo?: GoogleUserInfo
+  test: any
+
+  //css things
+  blurredActive: boolean = false
+
+  constructor(private readonly googleApi: GoogleApiService, private router: Router, private loginService: LoginService ) {}
 
   ngOnInit(): void {
   }
 
   loginWithGoogle() {
     this.googleApi.login()
+    this.googleApi.userProfileSubject.subscribe( info=>{
+      this.userInfo = info
+      console.log(info)
+      this.loginService.loginOrRegisterGoogleUser(info).subscribe((res: any) => {
+        this.test = res
+      }, 
+      err => {
+        console.log(err)
+      })
+    })
   }
 
   isLoggedIn(): boolean {
     return this.googleApi.isLoggedIn()
+  }
+  
+  logout() {
+    this.googleApi.signOut()
   }
 
 }
