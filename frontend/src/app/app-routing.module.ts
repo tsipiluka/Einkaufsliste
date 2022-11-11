f53172c20835b05482892314aa5841483056002a
DWsRekt <thedavidlord@yahoo.de>
2022-11-11 16:02:21 +0100

    Added routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ListOverviewComponent } from './components/list-overview/list-overview.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'registration',
+    component: RegistrationComponent
+  },
+  {
+    path: 'list-overview',
+    component: ListOverviewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
