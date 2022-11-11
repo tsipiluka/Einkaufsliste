feac55224726d4bd28a7dc689d64a325287c955e
DWsRekt <thedavidlord@yahoo.de>
2022-11-11 16:04:55 +0100

    Changed app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component'; 
 import {HttpClientModule} from '@angular/common/http'
 import { OAuthModule} from 'angular-oauth2-oidc';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { ListOverviewComponent } from './components/list-overview/list-overview.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    ListOverviewComponent
   ],
   imports: [
     BrowserModule,
