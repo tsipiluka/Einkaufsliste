920cb31db838b4344a68bc58da792cbcf74d93c8
DWsRekt <thedavidlord@yahoo.de>
2022-11-11 16:03:45 +0100

    Added List-Overview Component

---
+++ frontend/src/app/components/list-overview/list-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-overview',
+  templateUrl: './list-overview.component.html',
+  styleUrls: ['./list-overview.component.css']
+})
+export class ListOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
