88e797c28ac95a6600c13012fbdf21834af00cb3
Moritz <moritz.rfs@gmail.com>
2022-11-16 20:21:58 +0100

    Fix readsecrets

--- einkaufsliste_backend/core/read_secrets.py
+++ einkaufsliste_backend/core/read_secrets.py
@@ -1,10 +1,11 @@
 import os
 import json
 
-def read_secrets(attribute):
-    filename = os.path.join('secrets.json')
-    try:
-        with open(filename, mode='r') as f:
-            return json.load(f)[attribute]
-    except FileNotFoundError:
-        return {}
+class ReadSecrets:
+    def read_secrets(attribute):
+        filename = os.path.join('secrets.json')
+        try:
+            with open(filename, mode='r') as f:
+                return json.load(f)[attribute]
+        except FileNotFoundError:
+            return {}
