c061f444c4d3173ef2d576c0e12d54626534c3f1
Moritz <moritz.rfs@gmail.com>
2022-11-19 00:21:28 +0100

    Fix shoppinglist_contributors_add

--- einkaufsliste_backend/shoppinglist/urls.py
+++ einkaufsliste_backend/shoppinglist/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('shoppinglist/<int:id>/entries/add/', ShoppingListEntryAdd.as_view(), name='shoppinglist_entry_add'),
     path('shoppinglist/<int:shopping_list_id>/entry/<int:entry_id>/', ShoppingListEntryDetails.as_view(), name='shoppinglist_entry_details'),
     path('shoppinglist/<int:id>/contributors/', ShoppingListContributors.as_view(), name='shoppinglist_contributors'),
-    path('shoppinglist/<int:id>/contributors/add/', ShoppingListContributors.as_view(), name='shoppinglist_contributors_add'),
+    path('shoppinglist/<int:shopping_list_id>/contributors/add/', ShoppingListContributors.as_view(), name='shoppinglist_contributors_add'),
     path('shoppinglist/contributor/<int:contributor_id>', ShoppingListContributors.as_view(), name='shoppinglist_contributors_remove'),
 ]
