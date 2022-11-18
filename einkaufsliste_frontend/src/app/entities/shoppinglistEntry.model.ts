293f57cd42aa6949629b88693dfa212a5a631803
DWsRekt <thedavidlord@yahoo.de>
2022-11-18 23:58:03 +0100

    Changed ShoppinglistEntry Model

--- einkaufsliste_frontend/src/app/entities/shoppinglistEntry.model.ts
+++ einkaufsliste_frontend/src/app/entities/shoppinglistEntry.model.ts
@@ -5,7 +5,7 @@ export interface IShoppinglistEntry {
     id: number,
     name: string,
     status: string,
-    assignee: string,
+    assignee: User,
     shopping_list: Shoppinglist,
     creator: User
 }
@@ -15,7 +15,7 @@ export class ShoppinglistEntry implements IShoppinglistEntry{
         public id: number,
         public name: string,
         public status: string,
-        public assignee: string,
+        public assignee: User,
         public shopping_list: Shoppinglist, 
         public creator: User 
     ){}
