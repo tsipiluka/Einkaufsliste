8e7510d61c55398a7c117f2f2d63e7b791fd928f
Moritz <moritz.rfs@gmail.com>
2022-11-19 16:37:02 +0100

    Fix check_user_owner_contribution again

--- einkaufsliste_backend/shoppinglist/utils.py
+++ einkaufsliste_backend/shoppinglist/utils.py
@@ -14,7 +14,7 @@ def get_user_from_token(request):
         return None
 
 def check_user_owner_contribution(user, shopping_list):
-    if user == shopping_list.owner or ShoppingListContributor.objects.filter(user=user, shopping_list=shopping_list).exists():
+    if user == shopping_list.owner or ShoppingListContributor.objects.filter(shopping_list=shopping_list, contributor=user).exists():
         return True
     else:
         return False
