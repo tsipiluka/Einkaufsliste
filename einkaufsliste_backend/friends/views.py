f9e4ad69c0db87465e42b3881a22acf0c7b428b4
Moritz <moritz.rfs@gmail.com>
2022-11-19 14:00:05 +0100

    Update friends comments to be more precise

--- einkaufsliste_backend/friends/views.py
+++ einkaufsliste_backend/friends/views.py
@@ -9,6 +9,9 @@ from rest_framework.response import Response
 from rest_framework import status
 
 def get_user_from_token(request):
+    '''
+    Get user from token of the current request.
+    '''
     header = request.headers
     token = header.get('Authorization')
     try:
@@ -31,7 +34,9 @@ class Friends(APIView):
     
     def post(self, request):
         """
-        Implements an endpoint to create a new friend in the context of the currently logged in user.
+        Implements an endpoint to create a new friend in the context 
+        of the currently logged in user. Returns an error if the user
+        is already a friend of the other user.
 
         The endpoint expects a JSON object containing:
 
@@ -41,7 +46,6 @@ class Friends(APIView):
         if user is None:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
         serializer = FriendSerializer(data=request.data)
-
         # check if friendship already exists
         if not Friend.objects.filter(initiator=user, friend=request.data['friend']).exists():
             if serializer.is_valid():
@@ -55,7 +59,15 @@ class FriendDetails(APIView):
  
     def get(self, request, id):
         '''
-        Implements an endpoint to get a specific friend of the currently logged in user.
+        Implements an endpoint to get a specific friendship of the
+        currently logged in user. Returns an error if the friendship
+        does not exist.
+
+        The endpoint returns a JSON object containing:
+
+        * id: The id of the friendship.
+        * initiator: The id of the initiator of the friendship.
+        * friend: The id of the friend of the friendship.
         '''
         user = get_user_from_token(request)
         if user is None:
@@ -87,9 +99,15 @@ class FriendDetails(APIView):
     
     def delete(self, request, id):
         '''
-        Implements an endpoint to update a specific friend of the currently logged in user.
+        Implements an endpoint to update a specific frienship relation
+        of the currently logged in user. Expects the id of the friendship
+        to be deleted in the URL. Returns an error if the friendship does
+        not exist.
         '''
         user = get_user_from_token(request)
-        friend = Friend.objects.get(id=id, user=user)
-        friend.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        try:
+            friend = Friend.objects.get(id=id, user=user)
+            friend.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Friend.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
