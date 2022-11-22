6b4eeaea6e54df0e5cc6f95e8df25f21a60eea6d
Moritz <moritz.rfs@gmail.com>
2022-11-22 20:04:45 +0100

    Add possibility to add friend by username

--- einkaufsliste_backend/friends/urls.py
+++ einkaufsliste_backend/friends/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import Friends, FriendDetails
+from .views import FriendAdd, Friends, FriendDetails
 
-app_name = 'shoppinglist'
+app_name = "shoppinglist"
 
 urlpatterns = [
     # path('friends/', friends, name='friends'),
     # path('friend_details/<int:id>/', friendDetails, name='friend'),
-    path('friends/', Friends.as_view(), name='friends'),
-    path('friends/<int:id>/', FriendDetails.as_view(), name='friends_details'),
+    path("friends/", Friends.as_view(), name="friends"),
+    path("friends/<int:id>/", FriendDetails.as_view(), name="friends_details"),
+    path("friend/<str:user_name>/", FriendAdd.as_view(), name="friend_add"),
 ]
