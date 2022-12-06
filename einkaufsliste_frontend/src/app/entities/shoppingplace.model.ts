41678efcbc80e1007d45f5e16caf55374a3d4035
tsipiluka <72984318+tsipiluka@users.noreply.github.com>
2022-12-06 16:02:47 +0100

    Redesigned Maps integration in Shoppinglist

--- einkaufsliste_frontend/src/app/entities/shoppingplace.model.ts
+++ einkaufsliste_frontend/src/app/entities/shoppingplace.model.ts
@@ -1,11 +1,8 @@
 export interface IShoppingplace {
-  id: number;
   name: string;
-  address: string;
-  latitude: number;
-  longitude: number;
+  formatted_address: string;
 }
 
 export class Shoppingplace implements IShoppingplace {
-  constructor(public id: number, public name: string, public address: string, public latitude: number, public longitude: number) {}
+  constructor(public name: string, public formatted_address: string) {}
 }
