33ddb64371ed58c43162a05daab2cb921aefab10
DWsRekt <thedavidlord@yahoo.de>
2022-12-13 23:52:43 +0100

    Finished Registration Component

--- einkaufsliste_frontend/src/app/components/registration/registration.component.ts
+++ einkaufsliste_frontend/src/app/components/registration/registration.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { UserRegistration } from 'src/app/entities/user-registration.model';
 import { GoogleApiService } from 'src/app/google-api.service';
+import { ValidateInputService } from 'src/app/services/validate-input/validate-input.service';
 import { RegistrationService } from './service/registration.service';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
 })
 export class RegistrationComponent implements OnInit {
 
-  email: string = ''
-  username: string = ''
-  password: string = ''
+  email: string | undefined
+  username: string | undefined
+  new_password1: string | undefined
+  new_password2: string | undefined
 
-  constructor(private router: Router,private registrationService: RegistrationService, private googleApiService: GoogleApiService) { 
+  constructor(private router: Router,private registrationService: RegistrationService, private googleApiService: GoogleApiService,
+    private validateInputService: ValidateInputService, private messageService: MessageService)  { 
     if(localStorage.getItem('access_token')){
       this.router.navigate(['list-overview'])
     }
@@ -24,19 +28,57 @@ export class RegistrationComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showWarnMsg(msg: string) {
+    this.messageService.add({ severity: 'warn', summary: 'Warn', detail: msg });
+  }
+
+  showSuccessMsg(msg: string) {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: msg });
+  }
+
   googleLogin() {
     this.googleApiService.loginWithGoogle()
   }
 
   signUp() {
-    const user_credentials = {... new UserRegistration(this.email, this.username, this.password)}
-    this.registrationService.signUpUser(user_credentials).subscribe((res: any) => {
-      localStorage.setItem('access_token', res.access_token)
-      localStorage.setItem('refresh_token', res.refresh_token)
-      this.router.navigate(['list-overview'])
-    },
-    err => {
-      console.log(err)
-    })
+    if(this.validateInputService.validateEmail(this.email!)){
+      if(this.validateStringInput(this.username!)){
+        if(this.validateInputService.validatePassword(this.new_password1!)){
+          if(this.new_password1 === this.new_password2){
+            const user_credentials = {... new UserRegistration(this.email!, this.username!, this.new_password1!)}
+            this.registrationService.signUpUser(user_credentials).subscribe(async (res: any) => {
+              this.showSuccessMsg('Die Registrierung war erfolgreich! Sie werden weitergeleitet')
+              localStorage.setItem('access_token', res.access_token)
+              localStorage.setItem('refresh_token', res.refresh_token)
+              await this.delay(1000)
+              this.router.navigate(['list-overview'])
+            },
+            err => {
+              this.showWarnMsg('Die Registrierung ist fehlgeschlagen!')
+            })
+          }else{
+            this.showWarnMsg('Die Passwörter stimmen nicht überein!')
+          }
+        }else{
+          this.showWarnMsg('Bitte geben Sie ein gültiges Passwort ein! mindestens 8 Zeichen, 1 Großbuchstabe, 1 Kleinbuchstabe, 1 Sonderzeichen')
+        }
+      }else{
+        this.showWarnMsg('Bitte geben Sie einen gültigen Benutzernamen ein!')
+      }
+    }else{
+      this.showWarnMsg('Bitte geben Sie eine gültige E-Mail Adresse ein!')
+    }
+  }
+
+  validateStringInput(str: string) {
+    return str !== '' && str !== undefined && str !== null;
+  }
+
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
+
+  redirectToLogin() {
+    this.router.navigate(['login'])
   }
 }
