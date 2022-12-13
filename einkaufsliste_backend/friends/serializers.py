7bd26d60f9bd1edf6a4993a09521d246c9525200
Moritz <moritz.rfs@gmail.com>
2022-12-14 00:01:16 +0100

    Add new friend model

--- einkaufsliste_backend/friends/serializers.py
+++ einkaufsliste_backend/friends/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from .models import Friend
+from friends.models import Friend
 
 
 class FriendSerializer(serializers.ModelSerializer):
     class Meta:
         model = Friend
-        fields = ('id', 'initiator', 'friend')
+        fields = "__all__"
