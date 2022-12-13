1fd3a33bafb48df29d21574b44af9c32cb8aa6bc
tsipiluka <72984318+tsipiluka@users.noreply.github.com>
2022-12-13 14:49:08 +0100

    Change Env variables

--- einkaufsliste_backend/core/settings.py
+++ einkaufsliste_backend/core/settings.py
@@ -98,9 +98,9 @@ WSGI_APPLICATION = "core.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME", "postgres"),
-        "USER": os.environ.get("DB_USER", "postgres"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "NAME": os.environ.get("POSTGRES_DB", "postgres"),
+        "USER": os.environ.get("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "HOST": os.environ.get("DB_HOST", "localhost"),
         "PORT": os.environ.get("DB_PORT", "5432"),
     }
