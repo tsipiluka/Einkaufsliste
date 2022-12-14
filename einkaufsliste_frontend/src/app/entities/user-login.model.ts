a0cecce87a78dadb6a136a14bf8e98b258fc6e36
DWsRekt <thedavidlord@yahoo.de>
2022-12-14 12:42:40 +0100

    Remove cliendid and clientsecret from Userlogin Modal

--- einkaufsliste_frontend/src/app/entities/user-login.model.ts
+++ einkaufsliste_frontend/src/app/entities/user-login.model.ts
@@ -1,6 +1,4 @@
 export interface IUserLogin {
-    client_id: string,
-    client_secret: string,
     grant_type: string,
     username: string,
     password: string
@@ -8,8 +6,6 @@ export interface IUserLogin {
    
 export class UserLogin implements IUserLogin{
     constructor(
-        public client_id: string,
-        public client_secret: string,
         public grant_type: string,
         public username: string,
         public password: string
