5f07521ffa88f2aa9a186a02c4dae9a98342aa79
DWsRekt <thedavidlord@yahoo.de>
2022-12-14 12:51:43 +0100

    Removed clientid and clientsecret from google api service

--- einkaufsliste_frontend/src/app/entities/token-authentication.model.ts
+++ einkaufsliste_frontend/src/app/entities/token-authentication.model.ts
@@ -1,17 +1,13 @@
 export interface ITokenAuthentication {
     token: string,
     backend: string,
-    grant_type: string,
-    // client_id: string,
-    // client_secret: string
+    grant_type: string
 }
    
 export class TokenAuthentication implements ITokenAuthentication{
     constructor(
         public token: string,
         public backend: string,
-        public grant_type: string,
-        // public client_id: string,
-        // public client_secret: string
+        public grant_type: string
     ){}
 }
