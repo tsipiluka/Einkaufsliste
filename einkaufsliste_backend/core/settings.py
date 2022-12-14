ec5584f5625f1456c8dbd0b7cb34d044c6dd806e
Moritz <moritz.rfs@gmail.com>
2022-12-14 22:40:41 +0100

    Fix ALLOWED_HOST

--- einkaufsliste_backend/core/settings.py
+++ einkaufsliste_backend/core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get("FRONTEND_URL", "*")]
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOST", "*")]
 
 
 # Application definition
