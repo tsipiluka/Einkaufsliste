4cfbae2d383839d11e6cc4a7f945db22912f2a5c
DWsRekt <thedavidlord@yahoo.de>
2022-12-15 03:23:55 +0100

    Fixed List Overview Service Test

--- einkaufsliste_frontend/src/app/components/list-overview/service/list-overview.service.spec.ts
+++ einkaufsliste_frontend/src/app/components/list-overview/service/list-overview.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ListOverviewService } from './list-overview.service';
@@ -6,7 +7,9 @@ describe('ListOverviewService', () => {
   let service: ListOverviewService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(ListOverviewService);
   });
 
